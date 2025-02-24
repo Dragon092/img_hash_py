9b5ae14ab72c08ba75ae9de7b641ac172cf87892
Kevin Birke <kevin@birke.dev>
2025-02-24 23:33:44 +0100

    Add hash_image function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+use img_hash::{HasherConfig, HashAlg};
+use image::DynamicImage;
 
-/// Formats the sum of two numbers as string.
+/// Converts a raw image buffer from Python into an image::DynamicImage
+fn numpy_to_image(image_bytes: &[u8]) -> PyResult<DynamicImage> {
+    image::load_from_memory(image_bytes)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Failed to decode image: {}", e)))
+}
+
+/// Hash an image using img_hash and return an 8-byte hash
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn hash_image(py: Python, image_bytes: &[u8]) -> PyResult<PyObject> {
+    let img = numpy_to_image(image_bytes)?;
+    
+    let hasher = HasherConfig::with_bytes_type::<[u8; 8]>()
+        .hash_alg(HashAlg::Gradient)
+        .hash_size(8, 8)
+        .preproc_dct()
+        .to_hasher();
+
+    let hash = hasher.hash_image(&img);
+    
+    Ok(PyBytes::new(py, &hash.as_bytes()).into())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn img_hash_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(hash_image, m)?)?;
     Ok(())
 }
